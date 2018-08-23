d176bf5f8f2f612ff5db46d09ba2b919f9ccacfd
Zac Morris <zac@zacwolf.com>
2018-08-22 21:23:28 -0400

    Updated Grunt

--- install_grunt.bat
+++ install_grunt.bat
@@ -1,5 +1,11 @@
-npm install grunt
-npm install grunt-contrib-clean
-npm install grunt-string-replace
-npm install grunt-contrib-uglify
-npm install grunt-css
+call npm install -g grunt-cli
+echo "Installing grunt"
+call npm install grunt --save-dev
+echo "Installing grunt-contrib-clean"
+call npm install grunt-contrib-clean --save-dev
+echo "Installing grunt-string-replace"
+call npm install grunt-string-replace --save-dev
+echo "Installing grunt-contrib-uglify"
+call npm install grunt-contrib-uglify --save-dev
+echo "Installing grunt-css"
+call npm install grunt-css --save-dev
